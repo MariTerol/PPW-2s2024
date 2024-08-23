fd97ec03214b9f9612b0acfe313196cb5b8d503c
MariTerol <mariananterol@gmail.com>
2024-08-23 10:26:27 -0300

    add

---
+++ atividades/atividade_2/verificarNumeroPrimo.js
@@ -0,0 +1,15 @@
+/**
+ * Verifica se um numero é par
+ * @param {integer} n 
+ * @returns {boolean}
+ */
+function verificarNumeroPrimo(n){
+    if(n%i == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(verificarNumeroPrimo(2));
+console.log(verificarNumeroPrimo(3));
