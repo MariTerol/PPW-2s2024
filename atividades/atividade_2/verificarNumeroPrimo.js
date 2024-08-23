891a0ba47c5c26d55e349af42f1ba5d0a077518b
Mariana Terol <111513262+MariTerol@users.noreply.github.com>
2024-08-23 11:51:34 -0300

    Update verificarNumeroPrimo.js

--- atividades/atividade_2/verificarNumeroPrimo.js
+++ atividades/atividade_2/verificarNumeroPrimo.js
@@ -1,15 +1,23 @@
 /**
- * Verifica se um numero é par
+ * Verifica se um numero é primo
  * @param {integer} n 
  * @returns {boolean}
  */
 function verificarNumeroPrimo(n){
-    if(n%i == 0){
-        return true;
-    }else{
-        return false;
-    }
+ 
+ if (n<=1) return false;
+ if(n==2) return true;
+ if(n%2==0) return false;
+ 
+ for(let i=3;i<= Math.sqrt(n);i+=2){
+     if(n%i==0){
+         return false;
+     }
+ }
+    return true;
 }
 
-console.log(verificarNumeroPrimo(2));
-console.log(verificarNumeroPrimo(3));
+ let n = prompt("Informe um numero: ");
+ 
+ let resultado = verificarNumeroPrimo(n);
+ console.log(`O numero ${n} é primo? ${resultado}`);
